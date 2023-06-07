24b539d6005c103e894189309090777615c9c4ce
Hassaan bin Nasir <110283447+HassaanShaheen@users.noreply.github.com>
2023-06-07 21:15:35 +0500

    Update Image_text.dart

--- Image_text.dart
+++ Image_text.dart
@@ -1,65 +1,100 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class Image_to_text extends StatefulWidget {
-  const Image_to_text({Key? key}) : super(key: key);
+class ImageToText extends StatefulWidget {
+  const ImageToText({Key? key}) : super(key: key);
 
   @override
-  State<Image_to_text> createState() => _Image_to_textState();
+  State<ImageToText> createState() => _ImageToTextState();
 }
 
-class _Image_to_textState extends State<Image_to_text> {
+class _ImageToTextState extends State<ImageToText> {
   String result = "";
   File? image;
+  List<File> imageHistory = [];
+  CroppedFile? croppedFile;
   late Future<File> imageFile;
   ImagePicker? imagePicker;
 
-  performImageLabelling()async
-  {
-    final FirebaseVisionImage firebaseVisionImage =FirebaseVisionImage.fromFile(image);
+  performImageLabeling() async {
+    final FirebaseVisionImage firebaseVisionImage =
+    FirebaseVisionImage.fromFile(image!);
     final TextRecognizer recognizer = FirebaseVision.instance.textRecognizer();
     VisionText visionText = await recognizer.processImage(firebaseVisionImage);
-    result="";
+    result = "";
     setState(() {
-      for(TextBlock block in visionText.blocks)
-      {
-        // final String txt = block.text;
-        for(TextLine line in block.lines)
-        {
-          result +="${line.text}\n";
-          // for(TextElement element in line.elements)
-          // {
-          //   result +="${element.text} ";
-          // }
+      for (TextBlock block in visionText.blocks) {
+        for (TextLine line in block.lines) {
+          result += "${line.text}\n";
         }
-        // result += "\n\n";
       }
     });
   }
-  pickImageFromGallary() async {
-    XFile? pickedFile =await imagePicker?.pickImage(source: ImageSource.gallery);
-    image = File(pickedFile!.path);
-    setState(() {
-      image;
-      performImageLabelling();
-    });
+
+  pickImageFromGallery() async {
+    XFile? pickedFile =
+    await imagePicker?.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      CroppedFile? croppedFile = (await ImageCropper().cropImage(
+        sourcePath: pickedFile.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+        ],
+        uiSettings: [
+          AndroidUiSettings(
+            toolbarTitle: 'Crop Image',
+            toolbarColor: Colors.blueAccent,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.square,
+            lockAspectRatio: true,
+          ),
+        ],
+      ));
+      if (croppedFile != null) {
+        setState(() {
+          image = File(croppedFile!.path);
+          imageHistory.add(image!);
+          performImageLabeling();
+        });
+      }
+    }
   }
 
   captureImageWithCamera() async {
-    XFile? pickedFile =await imagePicker?.pickImage(source: ImageSource.camera);
-    image = File(pickedFile!.path);
-    setState(() {
-      image;
-      performImageLabelling();
-    });
+    XFile? pickedFile =
+    await imagePicker?.pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      croppedFile = (await ImageCropper().cropImage(
+        sourcePath: pickedFile.path,
+        aspectRatioPresets: [
+          CropAspectRatioPreset.square,
+        ],
+        uiSettings: [
+          AndroidUiSettings(
+            toolbarTitle: 'Crop Image',
+            toolbarColor: Colors.blueAccent,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.square,
+            lockAspectRatio: true,
+          ),
+        ],
+      ));
+      if (croppedFile != null) {
+        setState(() {
+          image = File(croppedFile!.path);
+          imageHistory.add(image!);
+          performImageLabeling();
+        });
+      }
+    }
   }
 
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
     imagePicker = ImagePicker();
   }
@@ -67,112 +102,157 @@ class _Image_to_textState extends State<Image_to_text> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text('Image To Text Converter'),
-          centerTitle: true,
-          leading: const Icon(
-            Icons.home,
-            color: Colors.white,
-            size: 30,
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.info,
-                  color: Colors.white,
-                  size: 27,
-                ))
-          ],
-          backgroundColor: Colors.teal.shade800,
+      backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: const Text(
+          'Image To Text Converter',
+          style: TextStyle(fontSize: 25),
         ),
-        bottomNavigationBar: CurvedNavigationBar(
-            animationDuration: const Duration(milliseconds: 300),
-            backgroundColor: Colors.white,
-            color: Colors.teal.shade800,
-            index: 1,
-            onTap: (index) {
-              if (index == 0) {
-                pickImageFromGallary();
-              } else if (index == 2) {
-                captureImageWithCamera();
-              }
-            },
-            items: const [
-              Icon(
-                Icons.image,
-                size: 35,
-                color: Colors.white,
-              ),
-              Icon(
-                Icons.home,
-                size: 35,
-                color: Colors.white,
-              ),
-              Icon(
-                Icons.camera_alt,
-                size: 35,
-                color: Colors.white,
-              )
-            ]),
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(
-                bottom: MediaQuery.of(context).size.height * 0.2,
-              ),
-            ),
-            AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Container(
-                padding: const EdgeInsets.only(top: 30),
-                width: double.infinity,
-                height: double.infinity,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/home.png'))),
-                child: Stack(
-                  children: [
-                    Center(
-                      child: Container(
-                        child: image != null
-                            ? Image.file(
-                          image!,
-                          fit: BoxFit.cover,
+        leading: const Icon(
+          Icons.home,
+          color: Colors.white,
+          size: 45,
+        ),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    height: 750,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(top: 30),
+                          width: 250,
+                          height: 250,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/icon.png'),
+                            ),
+                          ),
+                          child: image != null
+                              ? Image.file(
+                            image!,
+                            fit: BoxFit.cover,
+                          )
+                              : const SizedBox(),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: 135,
+                          height: 55,
+                          decoration: const BoxDecoration(
+                            color: Colors.blueAccent,
+                          ),
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                context,
+                                'Converted',
+                                arguments: result,
+                              );
+                            },
+                            child: const Text(
+                              "Convert",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
                         )
-                            : const SizedBox(),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 115,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.teal.shade800,
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'translation',arguments: result);
-                    },
-                    child: const Text(
-                      "Convert",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+                      ],
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        )
+                  Container(
+                    height: 55,
+                    color: Colors.blueAccent,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: () async{
+                            Map<Permission, PermissionStatus> statuses = await [
+                              Permission.storage, Permission.camera,
+                            ].request();
+                            if(statuses[Permission.storage]!.isGranted && statuses[Permission.camera]!.isGranted){
+                              pickImageFromGallery();
+                            }                   },
+                          icon: const Icon(
+                            Icons.image,
+                            size: 45,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async{
+                            Map<Permission, PermissionStatus> statuses = await [
+                              Permission.storage, Permission.camera,
+                            ].request();
+                            if(statuses[Permission.storage]!.isGranted && statuses[Permission.camera]!.isGranted){
+                              pickImageFromGallery();
+                            }                   },
+                          icon: const Icon(
+                            Icons.camera_alt,
+                            size: 45,
+                            color: Colors.white,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: const Text('Image History'),
+                                content: SizedBox(
+                                  width: double.maxFinite,
+                                  height: 300,
+                                  child: ListView.builder(
+                                    itemCount: imageHistory.length,
+                                    itemBuilder: (context, index) {
+                                      return ListTile(
+                                        leading: Image.file(
+                                          imageHistory[index],
+                                          width: 50,
+                                          height: 50,
+                                        ),
+                                        title: Text(
+                                          'Image ${index + 1}',
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.history,
+                            size: 45,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
